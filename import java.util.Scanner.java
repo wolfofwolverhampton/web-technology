57f71c7af7328fd2802567381c073084133923b6
srijan_limbu <np03cs4a22024@heraldcollege.edu.np>
2022-12-06 15:25:16 +0545

    second commit

---
+++ import java.util.Scanner.java
@@ -0,0 +1,13 @@
+import java.util.Scanner
+public class {
+    public static main void(String []args)
+    Scanner sc= new Scanner (System.in);
+    System.out.println("l=")
+    int l = sc.nextInt();
+    System.out.println("b=")
+    int b = sc.nextInt();
+    int areaofrectangle = l * b;
+    System.out.println("area of rectangle="+areaofrectangle)
+
+    
+}
